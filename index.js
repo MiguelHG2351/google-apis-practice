1795bda680f4353e370cd826ffbf8a125e2a805b
Miguel Hernández Gaitan <miguelhernandezgaitan13@gmail.com>
2022-09-07 07:35:28 +0000

    log port

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const app = require("./server");
 
-app.listen(process.env.PORT ?? 3000, () => {
-  console.log("App listening on port 3000!");
+const port = process.env.PORT ?? 3000
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}!`);
 });
